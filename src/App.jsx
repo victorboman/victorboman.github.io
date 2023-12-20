ceb338d2afc13549eb86aad79df6f05875d902d9
Victor Boman <victorboman92@gmail.com>
2023-12-20 21:35:34 +0100

    Created new componets cards, cardlist and started on states for adding new cards

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,40 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import Incrementor from "./components/Incrementor/Incrementor";
+import Cardlist from "./components/Cardlist/Cardlist";
+
+/* const userObj = {
+  name: "user",
+  sureName: "user",
+  number: "+42131231231",
+  profile: "",
+}; */
+const myJobs = [
+  {
+    workName: "JCDecaux",
+    workIntro:
+      "Here I worked on various tasks, such as planning digital advertising.",
+  },
+  {
+    workName: "If Försäkringsbolag",
+    workIntro:
+      "Here I worked on various tasks, such as planning digital advertising.",
+  },
+  {
+    workName: "MTG Radio",
+    workIntro:
+      "Here I worked on various tasks, such as planning digital advertising.",
+  },
+];
 
 function App() {
+  const [jobs, addJobs] = useState(myJobs);
+  /*  useEffect(() => {
+    setString("Siddes Bodega");
+  }, [helloString]); */
   return (
-    <div>
-      <Incrementor initialValue={23} /> <h2>Hello World!</h2>
+    <div className="wrapper">
+      <Incrementor initialValue={23} />
+      <Cardlist myJobs={jobs} />
     </div>
   );
 }
